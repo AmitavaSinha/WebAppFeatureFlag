bb6ff10ef7bb8274321caa25f28d593fbe407667
Amitava Sinha <Amitava.Sinha@gmail.com>
2019-01-18 15:14:15 +0530

    Feature Flag Enabled in DEV & QA

---
+++ DatabaseConnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace WebFormsApplication
+{
+    public class DatabaseConnection
+    {
+
+        public static void OpenConnection(SqlConnection con)
+        {
+            string ConnString = System.Configuration.ConfigurationManager.ConnectionStrings["ShoppingCartDemoDBConnectionString"].ConnectionString;
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                //con.ConnectionString = "Data Source=LP-5CD720CW14\\SQLEXPRESS;Initial Catalog=SocialGoaldB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                con.ConnectionString = ConnString;
+                con.Open();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+        public static void CloseConnection(SqlConnection con)
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+    }
+}
